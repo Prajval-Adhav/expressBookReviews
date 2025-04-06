f255ec5dbc68c8ef4d53f48311e19fc57330d212
Prajval Adhav <prajval.adhav@centralogic.net>
2025-04-06 20:57:51 +0530

    initial commit

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  // Check if access token is present in the session
+  const accessToken = req.session.accessToken;
+
+  if (accessToken) {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized: No access token found" });
+  }
 });
  
 const PORT =5000;
